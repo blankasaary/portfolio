4da06d58b590d7751d7e295531ac283209a37dec
Saáry Blanka <blankasaary@edu.bme.hu>
2024-12-08 16:59:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const userImageContainer = document.getElementById('random-user');
-  const imageUrl = 'https://github.com/blankasaary/portfolio/blob/main/340062889_610776987767061_881245899614018749_n.jpg'; // Itt add meg a saját képed URL-jét
+  const imageUrl = 'https://drive.google.com/file/d/12tA3aRwXbwuxrMUf9HOw1RlRgcQrq1oV/view?usp=sharing'; // Itt add meg a saját képed URL-jét
 
   // Új img elem létrehozása
   const img = document.createElement('img');
@@ -10,4 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
   // Kép hozzáadása a random-user div-hez
   userImageContainer.appendChild(img);
 });
-
