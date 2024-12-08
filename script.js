10dfc2822163e7d3b61b943873eab6ae265ca7e0
Saáry Blanka <blankasaary@edu.bme.hu>
2024-12-08 16:53:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const userImageContainer = document.getElementById('random-user');
-  const imageUrl = 'YOUR_IMAGE_URL'; // Itt add meg a saját képed URL-jét
+  const imageUrl = 'https://drive.google.com/file/d/12tA3aRwXbwuxrMUf9HOw1RlRgcQrq1oV/view?usp=drive_link'; // Itt add meg a saját képed URL-jét
 
   // Új img elem létrehozása
   const img = document.createElement('img');
